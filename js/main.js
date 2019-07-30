2103b271902861d88ac886b872ee21e7be84afb0
Angela Laub <angela.laub@gmx.de>
2019-07-30 21:40:13 +0200

    add accessibility + some css changes

--- js/main.js
+++ js/main.js
@@ -161,6 +161,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + ", " + restaurant.cuisine_type + ", " + restaurant.neighborhood;
+
   li.append(image);
 
   const name = document.createElement('h1');
